4078a99b2046333be799302d8c84a5a94694839c
2021115102-Shashi_SB <118378061+SbShashi5@users.noreply.github.com>
2024-05-04 22:31:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,41 +6,23 @@ import streamlit as st
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from sklearn.preprocessing import MinMaxScaler
-import base64
 
-# Load the image
-def load_image(image_path):
-    with open(image_path, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode("utf-8")
-
-# Define the file path of your image
-image_path = "D:\\Stock-Predictions-Model-main\\bg1.jpg"  # Replace this with the actual file path of your image
-
-# Get the base64 representation of the image
-image_base64 = load_image(image_path)
-
-# Define Streamlit app
-st.header('Stock Market Analyzer')
 # Load the LSTM model
 model = load_model('D:\\Stock-Predictions-Model-main\\Stock Predictions Model.keras')
 
 # Define Streamlit app
 st.header('Stock Market Analyzer')
-
-# Set background image
 st.markdown(
     """
     <style>
     body {
-        background-image: url("D:\\Stock-Predictions-Model-main\\bg1.jpg");
+        background-image: url("file:///D:/Stock-Predictions-Model-main/bg1.jpg");
         background-size: cover;
     }
     </style>
     """,
     unsafe_allow_html=True
 )
-
 # User input for stock symbol
 available_stocks = ['TATAMOTORS.NS', 'AAPL', 'GOOG', 'MSFT', 'AMZN']  # Add more stocks as needed
 selected_stock = st.selectbox('Select Stock Symbol', available_stocks)
@@ -49,13 +31,8 @@ selected_stock = st.selectbox('Select Stock Symbol', available_stocks)
 start_date = st.date_input('Start Date', pd.to_datetime('2021-04-27'), format='YYYY/MM/DD')
 end_date = st.date_input('End Date', pd.to_datetime('today'), format='YYYY/MM/DD')
 
-
 # Fetch data using yfinance
 data = yf.download(selected_stock, start=start_date, end=end_date)
-
-# Display stock data
-st.subheader('Stock Data')
-st.write(data)
 
 # Preprocess data
 data_train = pd.DataFrame(data.Close[0: int(len(data)*0.80)])
@@ -83,24 +60,20 @@ scale = 1 / scaler.scale_
 predict = predict * scale
 y = y * scale
 
+# Create subplots
+fig = make_subplots(rows=3, cols=2, 
+                    subplot_titles=('Stock Prices', 'Volume', 'Moving Averages', 'RSI', 'MACD', 'Bollinger Bands'))
+
 # Plot actual stock prices as candlestick chart
-fig_candlestick = go.Figure()
-fig_candlestick.add_trace(go.Candlestick(x=data.index,
-                                         open=data['Open'],
-                                         high=data['High'],
-                                         low=data['Low'],
-                                         close=data['Close'],
-                                         name='Candlestick'))
-fig_candlestick.update_layout(title_text=f'Candlestick Chart for {selected_stock} ({start_date.strftime("%Y/%m/%d")} to {end_date.strftime("%Y/%m/%d")})',
-                              xaxis_title='Date', yaxis_title='Price', legend=dict(x=0, y=1))
-st.plotly_chart(fig_candlestick)
+fig.add_trace(go.Candlestick(x=data.index,
+                             open=data['Open'],
+                             high=data['High'],
+                             low=data['Low'],
+                             close=data['Close'],
+                             name='Candlestick'), row=1, col=1)
 
 # Plot volume
-fig_volume = go.Figure()
-fig_volume.add_trace(go.Bar(x=data.index, y=data['Volume'], name='Volume', marker_color='blue'))
-fig_volume.update_layout(title_text=f'Volume for {selected_stock} ({start_date.strftime("%Y/%m/%d")} to {end_date.strftime("%Y/%m/%d")})',
-                         xaxis_title='Date', yaxis_title='Volume', legend=dict(x=0, y=1))
-st.plotly_chart(fig_volume)
+fig.add_trace(go.Bar(x=data.index, y=data['Volume'], name='Volume', marker_color='blue'), row=1, col=2)
 
 # Calculate moving averages
 ma_50_days = data['Close'].rolling(50).mean()
@@ -108,17 +81,10 @@ ma_100_days = data['Close'].rolling(100).mean()
 ma_200_days = data['Close'].rolling(200).mean()
 
 # Plot Price vs Moving Averages
-fig_ma = go.Figure()
-
-fig_ma.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Price'))
-fig_ma.add_trace(go.Scatter(x=data.index, y=ma_50_days, mode='lines', name='MA50'))
-fig_ma.add_trace(go.Scatter(x=data.index, y=ma_100_days, mode='lines', name='MA100'))
-fig_ma.add_trace(go.Scatter(x=data.index, y=ma_200_days, mode='lines', name='MA200'))
-
-fig_ma.update_layout(title_text=f'Price vs Moving Averages for {selected_stock} ({start_date.strftime("%Y/%m/%d")} to {end_date.strftime("%Y/%m/%d")})',
-                     xaxis_title='Date', yaxis_title='Price', legend=dict(x=0, y=1), height=600)
-
-st.plotly_chart(fig_ma)
+fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Price'), row=2, col=1)
+fig.add_trace(go.Scatter(x=data.index, y=ma_50_days, mode='lines', name='MA50'), row=2, col=1)
+fig.add_trace(go.Scatter(x=data.index, y=ma_100_days, mode='lines', name='MA100'), row=2, col=1)
+fig.add_trace(go.Scatter(x=data.index, y=ma_200_days, mode='lines', name='MA200'), row=2, col=1)
 
 # Calculate RSI
 def calculate_rsi(data, window=14):
@@ -132,13 +98,9 @@ def calculate_rsi(data, window=14):
 rsi = calculate_rsi(data['Close'])
 
 # Plot RSI
-fig_rsi = go.Figure()
-fig_rsi.add_trace(go.Scatter(x=data.index, y=rsi, mode='lines', name='RSI'))
-fig_rsi.update_layout(title_text=f'Relative Strength Index (RSI) for {selected_stock} ({start_date.strftime("YYYY/MM/DD")} to {end_date.strftime("YYYY/MM/DD")})',
-                      xaxis_title='Date', yaxis_title='RSI', legend=dict(x=0, y=1))
-st.plotly_chart(fig_rsi)
+fig.add_trace(go.Scatter(x=data.index, y=rsi, mode='lines', name='RSI'), row=2, col=2)
 
-# Plot MACD
+# Calculate MACD
 def calculate_macd(data, short_window=12, long_window=26):
     short_ema = data['Close'].ewm(span=short_window, min_periods=1).mean()
     long_ema = data['Close'].ewm(span=long_window, min_periods=1).mean()
@@ -150,16 +112,9 @@ def calculate_macd(data, short_window=12, long_window=26):
 macd, signal, histogram = calculate_macd(data)
 
 # Plot MACD
-fig_macd = go.Figure()
-
-fig_macd.add_trace(go.Scatter(x=data.index, y=macd, mode='lines', name='MACD'))
-fig_macd.add_trace(go.Scatter(x=data.index, y=signal, mode='lines', name='Signal'))
-fig_macd.add_trace(go.Bar(x=data.index, y=histogram, name='Histogram', marker_color='rgba(0, 128, 0, 0.5)'))
-
-fig_macd.update_layout(title_text=f'MACD for {selected_stock} ({start_date.strftime("%Y/%m/%d")} to {end_date.strftime("%Y/%m/%d")})',
-                       xaxis_title='Date', yaxis_title='MACD', legend=dict(x=0, y=1), height=600)
-
-st.plotly_chart(fig_macd)
+fig.add_trace(go.Scatter(x=data.index, y=macd, mode='lines', name='MACD'), row=3, col=1)
+fig.add_trace(go.Scatter(x=data.index, y=signal, mode='lines', name='Signal'), row=3, col=1)
+fig.add_trace(go.Bar(x=data.index, y=histogram, name='Histogram', marker_color='#6EB13F'), row=3, col=1)
 
 # Calculate Bollinger Bands
 def calculate_bollinger_bands(data, window=20, num_std=2):
@@ -172,11 +127,13 @@ def calculate_bollinger_bands(data, window=20, num_std=2):
 upper_band, lower_band = calculate_bollinger_bands(data)
 
 # Plot Bollinger Bands
-fig_bb = go.Figure()
-fig_bb.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Price'))
-fig_bb.add_trace(go.Scatter(x=data.index, y=upper_band, mode='lines', name='Upper Bollinger Band'))
-fig_bb.add_trace(go.Scatter(x=data.index, y=lower_band, mode='lines', name='Lower Bollinger Band'))
+fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Price'), row=3, col=2)
+fig.add_trace(go.Scatter(x=data.index, y=upper_band, mode='lines', name='Upper Bollinger Band'), row=3, col=2)
+fig.add_trace(go.Scatter(x=data.index, y=lower_band, mode='lines', name='Lower Bollinger Band'), row=3, col=2)
 
-fig_bb.update_layout(title_text=f'Bollinger Bands for {selected_stock} ({start_date.strftime("YYYY/MM/DD")} to {end_date.strftime("YYYY/MM/DD")})',
-                     xaxis_title='Date', yaxis_title='Price', legend=dict(x=0, y=1))
-st.plotly_chart(fig_bb)
+# Update layout
+fig.update_layout(height=1500, width=1200, showlegend=False, title_text=f'Stock Analysis for {selected_stock}',
+                  margin=dict(l=40, r=40, t=100, b=40),
+                  paper_bgcolor="LightSteelBlue",
+                  plot_bgcolor="#352323")
+st.plotly_chart(fig)
